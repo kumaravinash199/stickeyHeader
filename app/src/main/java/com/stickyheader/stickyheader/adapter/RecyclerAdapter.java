f2c6247d5c4d9891bdb9862f0a405bf5423fb27b
OnePlatinum <kumaravinash199@gmail.com>
2017-07-27 15:13:12 +0545

    Project setup for Sticky Header.

--- app/src/main/java/com/stickyheader/stickyheader/adapter/RecyclerAdapter.java
+++ app/src/main/java/com/stickyheader/stickyheader/adapter/RecyclerAdapter.java
@@ -2,30 +2,52 @@ package com.stickyheader.stickyheader.adapter;
 
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.stickyheader.stickyheader.R;
 import com.stickyheader.stickyheader.models.Item;
 
 import java.util.ArrayList;
 
-public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.>{
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyViewHolder>{
 
-    RecyclerAdapter(ArrayList<Item> itemList){
+    ArrayList<Item> listItem;
 
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView title, description;
+
+        public MyViewHolder(View view) {
+            super(view);
+            title = (TextView) view.findViewById(R.id.title);
+            description = (TextView) view.findViewById(R.id.description);
+        }
+    }
+
+
+    public RecyclerAdapter(ArrayList<Item> itemList){
+        this.listItem=itemList;
     }
 
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_list_row, parent, false);
+
+        return new MyViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Item mItem = listItem.get(position);
+        holder.title.setText(mItem.title);
+        holder.description.setText(mItem.description);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listItem.size();
     }
 }
