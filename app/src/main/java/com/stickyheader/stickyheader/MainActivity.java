aa0d45542cb848c2432d80b8bff91918f4c20d9b
OnePlatinum <kumaravinash199@gmail.com>
2017-07-30 17:01:03 +0545

    sticky header without external library.

--- app/src/main/java/com/stickyheader/stickyheader/MainActivity.java
+++ app/src/main/java/com/stickyheader/stickyheader/MainActivity.java
@@ -2,25 +2,53 @@ package com.stickyheader.stickyheader;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+
+import com.stickyheader.stickyheader.adapter.RecyclerAdapter;
+import com.stickyheader.stickyheader.models.Item;
+import com.stickyheader.stickyheader.utils.HeaderItemDecoration;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
+    RecyclerView.LayoutManager mLayoutManager;
+    RecyclerAdapter mRecyclerAdapter;
+    ArrayList<Item> itemArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        itemArrayList=new ArrayList<Item>();
+        mLayoutManager= new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,
+                false);
         initViews();
         initData();
     }
 
     public void initViews(){
         mRecyclerView=(RecyclerView)findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerAdapter=new RecyclerAdapter(this,itemArrayList);
+
+        HeaderItemDecoration headerItemDecoration =new HeaderItemDecoration(mRecyclerView,(HeaderItemDecoration.StickyHeaderInterface) mRecyclerAdapter);
+        mRecyclerView.addItemDecoration(headerItemDecoration);
+        mRecyclerView.setAdapter(mRecyclerAdapter);
 
     }
     public  void initData(){
 
+        for(int i=0;i<100;i++){
+            Item mItem= new Item();
+            mItem.title="Item at "+i;
+            mItem.description="item description at "+i;
+            itemArrayList.add(mItem);
+        }
+        mRecyclerAdapter.notifyDataSetChanged();
     }
+
 }
--- app/src/main/java/com/stickyheader/stickyheader/adapter/RecyclerAdapter.java
+++ app/src/main/java/com/stickyheader/stickyheader/adapter/RecyclerAdapter.java
@@ -1,34 +1,85 @@
 package com.stickyheader.stickyheader.adapter;
 
 
+import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.stickyheader.stickyheader.R;
 import com.stickyheader.stickyheader.models.Item;
+import com.stickyheader.stickyheader.utils.HeaderItemDecoration;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyViewHolder>{
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyViewHolder> implements HeaderItemDecoration.StickyHeaderInterface{
 
     ArrayList<Item> listItem;
+    Context mContext;
+    HeaderItemDecoration.StickyHeaderInterface mStickyHeaderInterface;
+
+    @Override
+    public int getHeaderPositionForItem(int itemPosition) {
+        int headerPosition = 0;
+        do {
+            if (this.isHeader(itemPosition)) {
+                headerPosition = itemPosition;
+                break;
+            }
+            itemPosition -= 1;
+        } while (itemPosition >= 0);
+        return headerPosition;
+    }
+
+    @Override
+    public int getHeaderLayout(int headerPosition) {
+
+        return R.layout.item_list_row;
+
+    }
+
+    @Override
+    public void bindHeaderData(View header, int headerPosition) {
+
+      Item mItem=listItem.get(headerPosition);
+      TextView headerTitle=(TextView)  header.findViewById(R.id.title);
+        TextView headerDescription=(TextView)  header.findViewById(R.id.description);
+        LinearLayout headerlayoutView=(LinearLayout)header.findViewById(R.id.itemLayout);
+        headerTitle.setText(mItem.title);
+        headerDescription.setText(mItem.description);
+        headerlayoutView.setBackgroundColor(Color.parseColor("#2DA427"));
+    }
+
+    @Override
+    public boolean isHeader(int itemPosition) {
+        if(itemPosition==0 || itemPosition%10==0){
+           return true;
+        }else{
+          return false;
+        }
+    }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         public TextView title, description;
+        public LinearLayout itemLayout;
 
         public MyViewHolder(View view) {
             super(view);
             title = (TextView) view.findViewById(R.id.title);
             description = (TextView) view.findViewById(R.id.description);
+            itemLayout=(LinearLayout)view.findViewById(R.id.itemLayout);
         }
     }
 
 
-    public RecyclerAdapter(ArrayList<Item> itemList){
+    public RecyclerAdapter(Context context, ArrayList<Item> itemList){
         this.listItem=itemList;
+        this.mContext=context;
     }
 
     @Override
@@ -44,6 +95,11 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
         Item mItem = listItem.get(position);
         holder.title.setText(mItem.title);
         holder.description.setText(mItem.description);
+        if(position==0 || position%10==0){
+            holder.itemLayout.setBackgroundColor(Color.parseColor("#2DA427"));
+        }else{
+            holder.itemLayout.setBackgroundColor(Color.parseColor("#ffffff"));
+        }
     }
 
     @Override
